9fb7aea3b4ecb8639404b05fd5e4827faec8a156
brijeshagal <brijeshagal@gmail.com>
2025-04-27 15:00:28 +0530

    add route

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,7 @@
 import cors from "cors";
 import dotenv from "dotenv";
-import express from "express";
+import express, { Request, Response } from "express";
 import { connectDB } from "./config/db";
-import aiRouter from "./routes/aiRoutes";
 import authRoutes from "./routes/authRoutes";
 import taskRoutes from "./routes/taskRoutes";
 
@@ -14,9 +13,12 @@ app.use(express.json());
 
 connectDB();
 
+app.use("/", (req: Request, res: Response) => {
+  res.json(200);
+});
+app.use("/api/tasks", taskRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/auth", authRoutes);
-app.use("/api/ai", aiRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
